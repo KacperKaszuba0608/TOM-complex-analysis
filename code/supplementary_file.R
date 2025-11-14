d63fe1d0ccb6468bfdf8b6f0aec68bc53d6350a3
Kacper Kaszuba <117150358+KacperKaszuba0608@users.noreply.github.com>
2025-11-14 19:51:26 +0100

    Update supplementary_file.R

--- code/supplementary_file.R
+++ code/supplementary_file.R
@@ -4,10 +4,11 @@ suppressWarnings(source("prepare_the_data.R"))
 # RAW DATA FRAMES
 raw_dataset1 <- read_tsv("./data/raw_dataset1.tsv", show_col_types = FALSE)
 raw_dataset2 <- read.table("./data/raw_dataset2.txt", sep = "\t", header = TRUE, check.names = FALSE)
+raw_dataset3 <- data.frame()
 fkbp8_dataset <- vroom::vroom("./data/proteinGroups_fkbp8.txt", show_col_types = FALSE)
 
 # COMBINED DATA FRAME
-combined_df <- merge(cleaned_data, dataset2, by.x="dataset2_id", by.y="Protein IDs", all = TRUE) |>
+combined_df <- merge(cleaned_data, dataset2, by = "UNIPROT", all = TRUE) |>
   mutate(FC_22_ev = ifelse(Log2_enrichment_FLAG_EV > 2 & is.na(FC_22_ev), -5, FC_22_ev),
          Log2_enrichment_FLAG_EV = ifelse(FC_22_ev > 2 & is.na(Log2_enrichment_FLAG_EV), -5, Log2_enrichment_FLAG_EV),
          Gene = ifelse(!is.na(Gene), Gene, Gene_names)
@@ -45,32 +46,40 @@ combined_df$p_allmv <- unlist(sapply(1:nrow(combined_df), function(i) {
   ifelse(any(is.na(row)), row[which(!is.na(row))], metap::sumlog(as.numeric(row))$p)
 }))
 
-combined_df <- merge(combined_df, functional_df, by.x = "Gene", by.y = "Gene name", all.x = TRUE)
+combined_df <- merge(combined_df, functional_df, by = "UNIPROT", all.x = TRUE)
 
 # STOCHIOMETRIC DATA FRAME
-stochiometric_df <- merge(cleaned_data, mitocopies_df, by.x="mitocopies_id", by.y="Protein IDs") |>
-  mutate(`Log10 mean mito-copies per cell (≥2/3 Reps)` = gsub("NaN", NA, `Log10 mean mito-copies per cell (≥2/3 Reps)`),
-         `Log10 mean mito-copies per cell (≥2/3 Reps)` = as.numeric(`Log10 mean mito-copies per cell (≥2/3 Reps)`),
-         UniProt = gsub("-\\d", "", Simple_ID, perl = TRUE))
+stochiometric_df <- merge(cleaned_data, mitocopies_df, by = "UNIPROT", all.x = TRUE) |>
+  mutate(
+    mito_copies_class = case_when(
+      `Log10 mean mito-copies per cell (≥2/3 Reps)` > 5.8 & `Mitochondrial based on all evidence sources` == "mito" ~ "High abundant",
+      `Log10 mean mito-copies per cell (≥2/3 Reps)` > 5.2 & `Mitochondrial based on all evidence sources` == "other" ~ "High abundant",
+      `Log10 mean mito-copies per cell (≥2/3 Reps)` > 5.1 & `Log10 mean mito-copies per cell (≥2/3 Reps)` < 5.8 
+      & `Mitochondrial based on all evidence sources` == "mito" ~ "Moderate abundant",
+      `Log10 mean mito-copies per cell (≥2/3 Reps)` > 4 & `Log10 mean mito-copies per cell (≥2/3 Reps)` < 5.2 
+      & `Mitochondrial based on all evidence sources` == "other" ~ "Moderate abundant",
+      `Log10 mean mito-copies per cell (≥2/3 Reps)` < 5.1 & `Mitochondrial based on all evidence sources` == "mito" ~ "Low abundant",
+      `Log10 mean mito-copies per cell (≥2/3 Reps)` < 4 & `Mitochondrial based on all evidence sources` == "other" ~ "Low abundant",
+      TRUE ~ "Null value"
+    ),
+    is_significant = ifelse(p_22_XL.adj < 0.05, TRUE, FALSE),
+    annotate = ifelse(FC_22_ev_XL >= 3 & is_significant
+                      | Gene %in% c("TOMM40L")
+                      , TRUE, FALSE))
+
+small_tims <- c("TIM8B", "TIM13", "T10B", "TIM8A")
+stochiometric_df$UniProt <- gsub("-\\d", "", stochiometric_df$Simple_ID, perl = TRUE)
 
 stochiometric_df <- mitocarta |> filter(UniProt != "0") |>
   fuzzyjoin::fuzzy_right_join(stochiometric_df, by = c("UniProt" = "UniProt"),
                               match_fun = str_detect) |>
-  mutate(MitoCarta3.0_List = ifelse(is.na(MitoCarta3.0_List), "other", "mito"),
-         mito_copies_class = case_when(
-           `Log10 mean mito-copies per cell (≥2/3 Reps)` > 5.8 & MitoCarta3.0_List == "mito" ~ "High abundant",
-           `Log10 mean mito-copies per cell (≥2/3 Reps)` > 5.3 & MitoCarta3.0_List == "other" ~ "High abundant",
-           `Log10 mean mito-copies per cell (≥2/3 Reps)` > 5.1 & `Log10 mean mito-copies per cell (≥2/3 Reps)` < 5.8 
-           & MitoCarta3.0_List == "mito" ~ "Moderate abundant",
-           `Log10 mean mito-copies per cell (≥2/3 Reps)` > 4.5 & `Log10 mean mito-copies per cell (≥2/3 Reps)` < 5.3 
-           & MitoCarta3.0_List == "other" ~ "Moderate abundant",
-           `Log10 mean mito-copies per cell (≥2/3 Reps)` < 5.1 & MitoCarta3.0_List == "mito" ~ "Low abundant",
-           `Log10 mean mito-copies per cell (≥2/3 Reps)` < 4.5 & MitoCarta3.0_List == "other" ~ "Low abundant",
-           TRUE ~ "Null value"
-         ),
-         is_significant = ifelse((FC_22_ev > 1 | FC_22_ev_XL > 1) & (p_22 < 0.05 & p_22_XL < 0.05),
-                                 TRUE, FALSE),
-         annotate = ifelse(FC_22_ev_XL >= 2.4 & is_significant & mito_copies_class != "Null value", TRUE, FALSE))
+  mutate(MitoCarta3.0_SubMitoLocalization = replace_na(MitoCarta3.0_SubMitoLocalization, "Other"),
+         MitoCarta3.0_SubMitoLocalization = case_when(
+           Gene %in% small_tims ~ "IMS",
+           Gene %in% "TRABD" ~ "MOM",
+           MitoCarta3.0_SubMitoLocalization == "unknown" ~ "Other",
+           TRUE ~ MitoCarta3.0_SubMitoLocalization
+         ))
 
 # BOOST DATA FRAME
 only_FC <- only_FC |>
@@ -84,28 +93,34 @@ only_FC <- only_FC |>
 
 # README
 legend <- data.frame("Table_S1_Datasets" = c(
-  "(A) Raw Dataset 1. \n
-  Table containing the raw MS data from Msfragger.",
-  "(B) Raw Dataset 2. \n
-  Table containing the raw MS data from MaxQuant.",
-  "(C) Raw Dataset 3 \n
-  Table containing the raw MS data from MaxQuant for fkbp8 KD",
-  "(D) Dataset from \u00d6zdemir et al.", 
-  "(E) Related to Figures S1C, 2A, 3B, 4A, 5B. \n
+  "(A) Related to Figures S1c, 2a, 3b, 4a, S5a.
   Data used to plot the figures with information from both dataset and additional databases (MitoCarta3.0, MitoCop, HCOP).",
-  "(F) Related to Figure 5C. \n
+  "(B) Related to Figure 5. 
   Data based on the Msfragger file containing log2 fold changes and boost of the crosslinked fold change values.",
-  "(G) Related to Figure S3A. \n
-  Data based on the Msfragger file containing and MitoCop mito-copies information."))
+  "(C) Related to Figure S5b. 
+  Data based on the Msfragger file containing and MitoCop mito-copies information.",
+  "(D) Dataset 1,4. 
+  This table contains the mitochondrial isolation data from HEK293T cells obtained from FragPipe.",
+  "(E) Dataset 2. 
+  This table contains the mitochondrial isolation data from HEK293T cells obtained from MaxQuant.",
+  "(F) Dataset 3 
+  This table contains the mitochondrial isolation data from HEK293T cells obtained from pLink.",
+  "(G) Dataset from \u00d6zdemir et al. 
+  This table contains the columns used from the dataset by \u00d6zdemir et al.",
+  "(H) Dataset 5+6 
+  Table containing the raw MS data from MaxQuant for fkbp8 KD.")
+  )
 
 # SUPPLEMENTARY TABLE
 list_of_sheets <- list("Legend" = legend,
-                      "(A) Raw Dataset 1" = raw_dataset1,
-                      "(B) Raw Dataset 2" = raw_dataset2,
-                      "(C) Raw Dataset fkbp8" = fkbp8_dataset,
-                      "(D) \u00d6zdemir et al. Dataset" = tom20,
-                      "(E) Combined Dataset" = combined_df,
-                      "(F) Figure 5C dataset" = only_FC,
-                      "(G) Figure S3A dataset" = stochiometric_df)
+                      "(A) Dataset 1+2" = combined_df,
+                      "(B) Figure 5" = only_FC,
+                      "(C) Figure S5b" = stochiometric_df,
+                      "(D) Dataset 1,4" = raw_dataset1,
+                      "(E) Dataset 2" = raw_dataset2,
+                      "(F) Dataset 3" = raw_dataset3,
+                      "(G) \u00d6zdemir et al. Dataset" = tom20,
+                      "(H) Dataset 5,6" = fkbp8_dataset
+                      )
 
-openxlsx::write.xlsx(list_of_sheets, file = "supplementary_table_2.xlsx", keepNA=TRUE, na.string='NA')
+openxlsx::write.xlsx(list_of_sheets, file = "supplementary_table.xlsx", keepNA=TRUE, na.string='NA')
